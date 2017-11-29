a62cb79c191de4737616e3ee254fe986c88c0c01
marli220 <32062983+marli220@users.noreply.github.com>
2017-11-29 15:42:28 +0100

    Initial commit

---
+++ Assets/Scripts/MenuControllers/GamePlayController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePlayController : MonoBehaviour {
+    [SerializeField]
+
+    private GameObject PausePanel;
+
+    void Awake ()
+    {
+        PausePanel.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        PausePanel.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        PausePanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/TetrisObjectsScripts/TetrisObject.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisObject : MonoBehaviour {
+
+        float LastFall = 0f;
+
+        void Update()
+        {
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.position += new Vector3(-1, 0, 0);
+
+                if (IsValidGridPosition())
+                {
+                    UpdateMatrixGrid();
+                }
+                else
+                {
+                    transform.position += new Vector3(1, 0, 0);
+                }
+
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.position += new Vector3(1, 0, 0);
+
+                if (IsValidGridPosition())
+                {
+                    UpdateMatrixGrid();
+                }
+                else
+                {
+                    transform.position += new Vector3(-1, 0, 0);
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                transform.Rotate(new Vector3(0, 0, -90));
+
+                if (IsValidGridPosition())
+                {
+                    UpdateMatrixGrid();
+                }
+                else
+                {
+                    transform.Rotate(new Vector3(0, 0, 90));
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - LastFall >= 1)
+            {
+                transform.position += new Vector3(0, -1, 0);
+
+                if (IsValidGridPosition())
+                {
+                    UpdateMatrixGrid();
+                }
+                else
+                {
+                    transform.position += new Vector3(0, 1, 0);
+
+                    MatrixGrid.DeleteWholeRows();
+
+                    FindObjectOfType<Spawner>().SpawnRandom();
+                    enabled = false;
+                }
+                LastFall = Time.time;
+            }
+        }
+
+        bool IsValidGridPosition()
+        {
+            foreach (Transform child in transform)
+            {
+                Vector2 v = MatrixGrid.RoundVector(child.position);
+
+                if (!MatrixGrid.IsInsideBorder(v))
+                    return false;
+
+                if (MatrixGrid.grid[(int)v.x, (int)v.y] != null && MatrixGrid.grid[(int)v.x, (int)v.y].parent != transform)
+                    return false;
+            }
+            return true;
+        }
+
+        void UpdateMatrixGrid()
+        {
+            for (int y = 0; y < MatrixGrid.column; ++y)
+            {
+                for (int x = 0; x < MatrixGrid.row; ++x)
+                {
+                    if (MatrixGrid.grid[x, y] != null)
+                    {
+                        if (MatrixGrid.grid[x, y].parent == transform)
+                        {
+                            MatrixGrid.grid[x, y] = null;
+
+                        }
+                    }
+                }
+
+            } //removing children
+            foreach (Transform child in transform)
+            {
+                Vector2 v = MatrixGrid.RoundVector(child.position);
+                MatrixGrid.grid[(int)v.x, (int)v.y] = child;
+            } // adding new children
+        }
+
+
+    }
+
